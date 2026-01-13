ca98d80819e9b9fab11e42098e653dbdd81a737f
coderyeasin <yeasin3233@gmail.com>
2026-01-13 23:19:04 +0600

    polish

--- src/components/Banner/RightSection.tsx
+++ src/components/Banner/RightSection.tsx
@@ -46,9 +46,9 @@ const RightSection = () => {
           </div>
         </div>
       </aside>
-      <div className="absolute -bottom-28 to right-0 w-full p-6 bg-TextPrimary">
-        <div className="relative w-214 h-42.25">
-          <div className="absolute inset-0 bg-[url(/assets/images/play-shape.png)] bg-cover z-50 flex justify-center items-center">
+      <div className="absolute -bottom-22 right-0 w-full ">
+        <div className="relative w-214 h-42.25 border-14 border-TextPrimary p-2 bg-offWhite rounded-tr-[3rem] rounded-br-[3rem] rounded-bl-[3rem] rounded-tl-[9rem]">
+          <div className="flex justify-center items-center">
             <div className="bg-primary/70 h-28 w-28 rounded-full flex justify-center items-center">
               <p className="text-xl bg-secondary h-20 w-20 rounded-full flex justify-center items-center">
                 <FaPlay className="text-5xl" />
@@ -56,7 +56,7 @@ const RightSection = () => {
             </div>
           </div>
           {/* <p className="bg-TextPrimary w-14 h-14 rounded-tl-4xl absolute -top-9 left-11 rotate-19 z-30"></p> */}
-          <p className="absolute text-xl bg-darkSecondary h-34 w-20 -top-16.5 -left-14 rotate-35 z-10 "></p>
+          {/* <p className="absolute text-xl bg-darkSecondary h-34 w-20 -top-16.5 -left-14 rotate-35 z-10 "></p> */}
         </div>
       </div>
     </section>
