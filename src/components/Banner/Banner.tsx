03b4286090082335281b7aef8eb42dd72cfee89a
coderyeasin <yeasin3233@gmail.com>
2026-01-23 23:02:27 +0600

    polish

--- src/components/Banner/Banner.tsx
+++ src/components/Banner/Banner.tsx
@@ -3,7 +3,7 @@ import RightSection from "./RightSection";
 
 const Banner = () => {
   return (
-    <div className="w-full grid grid-cols-2">
+    <div className="w-full grid grid-cols-1 lg:grid-cols-2">
       {/* left section */}
       <LeftSection />
 
